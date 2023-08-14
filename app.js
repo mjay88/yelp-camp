e6e427eac9b47112c90012281a721f22947915c6
mjay88 <mwilliamkjarrett@gmail.com>
2023-08-14 18:14:20 -0500

    add morgan, add layout, add partials,  start styles

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground"); //import model from models folder
 //connect db
@@ -18,11 +20,13 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); //joins all views to the root route ('/')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
 	res.render("home");
