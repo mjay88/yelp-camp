dcdc1fe8549be3dfc8568ebc3f6eb0117371f80f
mjay88 <mwilliamkjarrett@gmail.com>
2023-08-15 18:53:56 -0500

    add client side validation, catchAsync and ExpressError in utils folder

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground"); //import model from models folder
+const ExpressError = require("./utils/ExpressError");
+const catchAsync = require("./utils/catchAsync");
 //connect db
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 	useNewUrlParser: true,
@@ -33,46 +35,69 @@ app.get("/", (req, res) => {
 });
 
 //campground index
-app.get("/campgrounds", async (req, res) => {
-	const campgrounds = await Campground.find({});
-	res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+	"/campgrounds",
+	catchAsync(async (req, res) => {
+		const campgrounds = await Campground.find({});
+		res.render("campgrounds/index", { campgrounds });
+	})
+);
 //route for serving form for new campground, has to go before details route
 app.get("/campgrounds/new", (req, res) => {
 	res.render("campgrounds/new");
 });
 //route to post new campground
-app.post("/campgrounds", async (req, res) => {
-	const campground = new Campground(req.body.campground);
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post(
+	"/campgrounds",
+	catchAsync(async (req, res, next) => {
+		const campground = new Campground(req.body.campground);
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
 
 //show/details route, order matters
-app.get("/campgrounds/:id", async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/show", { campground });
-});
+app.get(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		res.render("campgrounds/show", { campground });
+	})
+);
 
 //edit route to serve edit form
-app.get("/campgrounds/:id/edit", async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/edit", { campground });
-});
+app.get(
+	"/campgrounds/:id/edit",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		res.render("campgrounds/edit", { campground });
+	})
+);
 //put route for editting
-app.put("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
-	const campground = await Campground.findByIdAndUpdate(id, {
-		...req.body.campground,
-	});
-	res.redirect(`/campgrounds/${campground._id}`);
-});
+app.put(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const campground = await Campground.findByIdAndUpdate(id, {
+			...req.body.campground,
+		});
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
 
 //delete route
-app.delete("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
-	await Campground.findByIdAndDelete(id);
-	res.redirect("/campgrounds");
+app.delete(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		await Campground.findByIdAndDelete(id);
+		res.redirect("/campgrounds");
+	})
+);
+
+//error handleing
+app.use((err, req, res, next) => {
+	res.send("OH BOY, SOMETHING WENT WRONG");
 });
 
 app.listen(3000, () => {
