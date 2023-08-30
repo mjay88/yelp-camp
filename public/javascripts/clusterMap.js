8d59103e5b8ef0c4bdded595f504365e93ce246c
mjay88 <mwilliamkjarrett@gmail.com>
2023-08-30 13:48:07 -0500

    clean up styles

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 // const campgrounds = campgrounds;
 const map = new mapboxgl.Map({
-	container: "map",
+	container: "cluster-map",
 	// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 	style: "mapbox://styles/mapbox/light-v11",
 	center: [-103.5917, 40.6699],
@@ -113,4 +113,7 @@ map.on("load", () => {
 	map.on("mouseleave", "clusters", () => {
 		map.getCanvas().style.cursor = "";
 	});
+
+	// Add zoom and rotation controls to the map.
+	map.addControl(new mapboxgl.NavigationControl());
 });
